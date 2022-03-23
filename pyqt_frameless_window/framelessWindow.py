130b88e40cd7ba21ecd9d13db499caba150f333f
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-24 08:30:57 +0900

    Set minimum size correctly based on any window this covers

--- pyqt_frameless_window/framelessWindow.py
+++ pyqt_frameless_window/framelessWindow.py
@@ -17,7 +17,7 @@ class FramelessWindow(QWidget):
         self.__initBasicUi()
 
     def __initBasicUi(self):
-        self.setMinimumSize(60, 60)
+        self.setMinimumSize(self.sizeHint().width(), self.sizeHint().height())
         self.setMouseTracking(True)
         self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowMinMaxButtonsHint)
 
