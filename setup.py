3775ec7f1ce3e5f4a618123e4b8082867d1dd6b7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-28 15:31:22 +0900

    Update version (0.2.0 -> 0.2.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-frameless-window',
-    version='0.2.0',
+    version='0.2.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
