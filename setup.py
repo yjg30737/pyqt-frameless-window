7e8167299f6d427275023868a809aee81584f1cc
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 15:01:03 +0900

    Update setup.py to upload the package to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-frameless-window',
-    version='0.2.1',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description="PyQt Frameless window to inherit a variety of frameless widget",
     url='https://github.com/yjg30737/pyqt-frameless-window.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15'
     ]
