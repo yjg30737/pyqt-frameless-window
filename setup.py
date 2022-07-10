bb2c6a8ec3c9abd44be729dab2a2ef3e11c82786
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-10 09:15:43 +0900

    Bump version (0.0.1 -> 0.0.2)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='pyqt-frameless-window',
-    version='0.0.1',
+    version='0.0.2',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
