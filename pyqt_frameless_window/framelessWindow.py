96649f2c4edd123897f8d842ecc00fc1293c0839
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-28 16:00:41 +0900

    Add new function (setVerticalExpandedEnabled) and related variables

--- pyqt_frameless_window/framelessWindow.py
+++ pyqt_frameless_window/framelessWindow.py
@@ -14,6 +14,8 @@ class FramelessWindow(QWidget):
         self._margin = 3
         self._cursor = QCursor()
         self._pressToMove = False
+
+        self._verticalExpandedEnabled = False
         self._verticalExpanded = False
         self._originalY = 0
         self._originalHeightBeforeExpand = 0
@@ -93,46 +95,47 @@ class FramelessWindow(QWidget):
         return super().mousePressEvent(e)
 
     def mouseDoubleClickEvent(self, e):
-        p = e.pos()
+        if self._verticalExpandedEnabled:
+            p = e.pos()
 
-        rect = self.rect()
-        rect.setX(self.rect().x() + self._margin)
-        rect.setY(self.rect().y() + self._margin)
-        rect.setWidth(self.rect().width() - self._margin * 2)
-        rect.setHeight(self.rect().height() - self._margin * 2)
+            rect = self.rect()
+            rect.setX(self.rect().x() + self._margin)
+            rect.setY(self.rect().y() + self._margin)
+            rect.setWidth(self.rect().width() - self._margin * 2)
+            rect.setHeight(self.rect().height() - self._margin * 2)
 
-        y = p.y()
+            y = p.y()
 
-        y1 = self.rect().y()
-        y2 = self.rect().height()
+            y1 = self.rect().y()
+            y2 = self.rect().height()
 
-        top = abs(y - y1) <= self._margin # far top
-        bottom = abs(y - (y2 + y1)) <= self._margin # far bottom
+            top = abs(y - y1) <= self._margin # far top
+            bottom = abs(y - (y2 + y1)) <= self._margin # far bottom
 
-        ag = QDesktopWidget().availableGeometry()
+            ag = QDesktopWidget().availableGeometry()
 
-        # fixme minor bug - resizing after expand can lead to inappropriate result when in comes to expanding again, it should be fixed
-        # vertical expanding when double-clicking either top or bottom edge
-        # back to normal
-        if self._verticalExpanded:
-            if top or bottom:
-                self.move(self.x(), self._originalY)
-                self.resize(self.width(), self._originalHeightBeforeExpand)
-                self._verticalExpanded = False
-        # expand vertically
-        else:
-            if top or bottom:
-                self._verticalExpanded = True
-                min_size = self.minimumSize()
-                max_size = self.maximumSize()
-                geo = self.geometry()
-                self._originalY = geo.y()
-                self._originalHeightBeforeExpand = geo.height()
-                geo.moveTop(0)
-                self.setGeometry(geo)
-                self.setFixedHeight(ag.height()-2)
-                self.setMinimumSize(min_size)
-                self.setMaximumSize(max_size)
+            # fixme minor bug - resizing after expand can lead to inappropriate result when in comes to expanding again, it should be fixed
+            # vertical expanding when double-clicking either top or bottom edge
+            # back to normal
+            if self._verticalExpanded:
+                if top or bottom:
+                    self.move(self.x(), self._originalY)
+                    self.resize(self.width(), self._originalHeightBeforeExpand)
+                    self._verticalExpanded = False
+            # expand vertically
+            else:
+                if top or bottom:
+                    self._verticalExpanded = True
+                    min_size = self.minimumSize()
+                    max_size = self.maximumSize()
+                    geo = self.geometry()
+                    self._originalY = geo.y()
+                    self._originalHeightBeforeExpand = geo.height()
+                    geo.moveTop(0)
+                    self.setGeometry(geo)
+                    self.setFixedHeight(ag.height()-2)
+                    self.setMinimumSize(min_size)
+                    self.setMaximumSize(max_size)
 
         return super().mouseDoubleClickEvent(e)
 
@@ -198,3 +201,6 @@ class FramelessWindow(QWidget):
         b = QBrush(color)
         p.setBrush(QPalette.Window, b)
         self.setPalette(p)
+
+    def setVerticalExpandedEnabled(self, f: bool):
+        self._verticalExpandedEnabled = f
